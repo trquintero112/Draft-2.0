a2206fc56f5a74ef2c93dc8fce62f4e18f253a7f
trquintero112 <trquintero112@gmail.com>
2026-08-15 16:31:03 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+window.SUPABASE_URL = 'https://kprelmyazivrkckidvfg.supabase.co';
+window.SUPABASE_PUBLIC_KEY = 'sb_publishable_L-Nkx4qYeu5rbLxZWYbJNw_-whObqZv';
+window.SUPABASE_TABLE = 'fantasy_players';
